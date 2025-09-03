c0e0cba95b45b3cca4ddcae45f84278d05d5cf6c
bilaldev2 <bilal.developer.arcly@gmail.com>
2025-09-03 10:51:10 +0530

    Next Migration

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'leaflet/dist/leaflet.css';
-import 'react-leaflet-markercluster/dist/styles.min.css'; 
-import './index.css'
-import App from './App.jsx'
-createRoot(document.getElementById('root')).render(
-    <App />
-)
